f16d74534b7ed5f68ad197cec19d679ae9f35cf4
Igor Postnikov <igor.postnikov8@gmail.com>
2021-05-17 19:29:28 -0700

    Added full weather data

--- Clima/Model/WheaterManager.swift
+++ Clima/Model/WheaterManager.swift
@@ -8,8 +8,14 @@
 
 import Foundation
 
+protocol WeatherManagerDelegate {
+    func didUpdateWeather(weather: WeatherModel)
+}
+
 struct WeatherManager {
     let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=2a04ab0ecf20edcf54ecfda26486ed1c&units=metric"
+    
+    var delegate: WeatherManagerDelegate?
     
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
@@ -31,7 +37,9 @@ struct WeatherManager {
                 }
                 
                 if let safeData = data {
-                    parseJSON(weatherData: safeData)
+                    if let weather = self.parseJSON(weatherData: safeData) {
+                        self.delegate?.didUpdateWeather(weather: weather)
+                    }
                 }
             }
             
@@ -41,15 +49,52 @@ struct WeatherManager {
         }
     }
     
-    func parseJSON(weatherData: Data) {
+    func parseJSON(weatherData: Data) -> WeatherModel? {
         let decoder = JSONDecoder()
         do {
             let decodedData = try decoder.decode(WheaterData.self, from: weatherData)
-            print(decodedData.name)
-            print(decodedData.main.temp)
+            let id = decodedData.weather[0].id
+            let temp = decodedData.main.temp
+            let name = decodedData.name
+            
+            let weather = WeatherModel(conditionId: id, cityName: name, temperature: temp)
+            return weather
+            
+            print(weather.temperatureString)
+            
+//            print(decodedData.coord.lat)
+//            print(decodedData.coord.lon)
+//            print(decodedData.weather[0].id)
+//            print(decodedData.weather[0].main)
+//            print(decodedData.weather[0].description)
+//            print(decodedData.weather[0].icon)
+//            print(decodedData.base)
+//            print(decodedData.main.temp)
+//            print(decodedData.main.feels_like)
+//            print(decodedData.main.temp_min)
+//            print(decodedData.main.temp_max)
+//            print(decodedData.main.pressure)
+//            print(decodedData.main.humidity)
+//            print(decodedData.visibility)
+//            print(decodedData.wind.speed)
+//            print(decodedData.wind.deg)
+//            print(decodedData.clouds.all)
+//            print(decodedData.dt)
+//            print(decodedData.sys.type)
+//            print(decodedData.sys.id)
+//            print(decodedData.sys.country)
+//            print(decodedData.sys.sunrise)
+//            print(decodedData.sys.sunset)
+//            print(decodedData.timezone)
+//            print(decodedData.id)
+//            print(decodedData.name)
+//            print(decodedData.cod)
         } catch {
             print(error)
+            return nil
         }
     }
+    
+    
     
 }
